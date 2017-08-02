c3add1833fbb1d02ec4d96db79dc2ea8c2e51ea2
Joe-noh <goflb.jh@gmail.com>
2017-08-03 00:33:00 +0900

    we don;t need babel couse node premised
    Revert "use babel"
    
    This reverts commit 2d43a299f9cb14dd2e09187ff8e0fd93db31d6c9.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import nodeResolve  from 'rollup-plugin-node-resolve'
 
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
   plugins: [
-    nodeResolve({jsnext: true}),
-    babel()
+    nodeResolve({ jsnext: true }),
   ]
 }
